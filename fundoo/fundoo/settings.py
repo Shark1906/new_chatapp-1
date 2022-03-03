cd1546d91e6ac07e7c69cb33567dbd2533315005
Shark1906 <91650558+Shark1906@users.noreply.github.com>
2022-03-04 01:29:08 +0530

    Update settings.py

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'aman',
         'USER': 'admin',
-        'PASSWORD': 'shark1906',
+        'PASSWORD': 'shark1906@',
         'HOST': 'database-2.clrqzcwedelt.ap-south-1.rds.amazonaws.com',
         'PORT': '3306',
     }
